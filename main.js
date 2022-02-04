2b1a9a84646810b2f813dd52ff7b26dd03c8db9c
tom <anytable@gmail.com>
2022-02-04 21:54:23 +0800

    a demo of vite width photon-web

---
+++ main.js
@@ -0,0 +1,138 @@
+import './a.scss'
+import init, {open_image, putImageData, offset_red} from "photon-web";
+
+init().then(() => {
+    function adjustSize(sw, sh, tw, th) {
+        let r = 1
+        const r0 = sw / tw
+        const r1 = sh / th
+        if (r0 > 1 || r1 > 1) {
+            r = Math.max(r0, r1)
+        } else if (r0 < 1 && r1 < 1) {
+            r = Math.min(r0, r1)
+        }
+        const [w, h] = [sw / r, sh / r]
+        area = [(tw - w) / 2, (th - h) / 2, w, h]
+        return [0, 0, sw, sh, ...area]
+    }
+
+    let area = [0, 0, 0, 0]
+    const parseNodes = c => {
+        const tmp = document.createElement('div')
+        tmp.innerHTML = c;
+        return Array.from(tmp.childNodes)
+    }
+
+    const $ = (s, ...a) => {
+        const nodes = [];
+        const l = a.length
+        let root, pre
+        if (s) {
+            if (typeof s === 'string') {
+                if (s[0] === '<') {
+                    root = parseNodes(s)[0];
+                } else {
+                    root = document.querySelector(s);
+                }
+            } else {
+                if (s instanceof Node) root = s;
+            }
+            pre = root
+        }
+        if (root) for (let i = 0; i < l; i++) {
+            const c = a[i]
+            switch (Object.prototype.toString.call(c)[8]) {
+                case 'S':
+                    const cs = parseNodes(c)
+                    cs.forEach(e => root.appendChild(e));
+                    if (cs.length === 1) pre = cs[0];
+                    else pre = cs;
+                    break;
+                case 'N':
+                    pre = root.appendChild(document.createTextNode(c))
+                    break;
+                case 'H':
+                    pre = root.appendChild(c)
+                    break;
+                case 'F':
+                    c(pre)
+                    continue;
+                case 'O':
+                    Object.assign(pre, c)
+                    continue;
+            }
+            nodes.push(pre)
+        }
+        if (!nodes.length) return root;
+        return [root].concat(nodes)
+    }
+    let ctx1, ctx0
+    const [imgArea, ori, result, input, line] = $(
+        '<div class="img">',
+        '<canvas class="original">',
+        '<canvas class="result">',
+        '<input type="file"/>', {
+            onchange() {
+                processFile(this.files)
+            },
+            onmousemove(e) {
+                if (ctx1) ctx1.dr(e.offsetX)
+            }
+        },
+        '<div class="line">'
+    );
+
+
+    function processImg(img) {
+        const w = ori.width = result.width = ori.offsetWidth
+        const h = ori.height = result.height = ori.offsetHeight
+        ctx0 = ori.getContext('2d')
+        ctx1 = result.getContext('2d')
+        ctx1.dr = function (a) {
+            if (ctx1 && ctx1.img) {
+                ctx1.x = a;
+                ctx1.putImageData(ctx1.img, area[0], area[1])
+                ctx1.clearRect(0, 0, a, h)
+                line.style.transform = `translate3d(${a}px,0,0)`
+            }
+        }
+        img = img || ctx0.img
+        if (!img) return;
+        ctx0.img = img;
+        const {width, height} = img
+        ctx0.drawImage(img, ...adjustSize(width, height, w, h))
+        const img2 = open_image(ori, ctx0);
+        offset_red(img2, 20)
+        putImageData(result, ctx1, img2);
+        ctx1.img = ctx1.getImageData(...area)
+        ctx1.clearRect(0, 0, w, h)
+        ctx1.putImageData(ctx1.img, area[0], area[1])
+        ctx1.dr(0, ctx1.x)
+    }
+
+    function processFile(files) {
+        const f = [].find.call(files, ({type}) => /image/.test(type))
+        if (!f) return;
+        const img = new Image();
+        img.onload = function () {
+            processImg(img)
+        }
+        img.src = URL.createObjectURL(f)
+    }
+
+    const root = $('#app', {
+        ondragover(e) {
+            e.preventDefault()
+        },
+        async ondrop(e) {
+            e.preventDefault()
+            const {dataTransfer: {files}} = e;
+            processFile(files)
+        }
+    });
+    $(root, imgArea)
+    window.onreset = function () {
+        processImg()
+    }
+})
+
---
+++ src/drop.js
@@ -0,0 +1,3 @@
+export function onDrop(e){
+
+}
